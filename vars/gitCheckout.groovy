d5e6a0234d0f8feeb06343456bdb00e9fa45b283
manish joon <magic@manishs-MacBook-Pro.local>
2020-04-09 17:11:12 -0700

    adding Git code in pipeline

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -3,7 +3,7 @@ def call(Map stageParams) {
     checkout([
             $class: 'GitSCM',
             branches:[[ name: stageParams.branch]],
-            userRemoteConfigs: [[ url: stageParams.url]],
-            //git clone https://github.com/magicjoon/vertex-lib.git
+            userRemoteConfigs:[[ url: stageParams.url]]
     ])
-}
+    sh " git pull ${url}"
+}pwd
